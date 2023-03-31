67d0dc81d9ca2852531e9d8a1bd5094267a6ab07
Axesan <82097764+Axesan@users.noreply.github.com>
2023-03-31 16:12:33 +0200

    Add, test products

--- front/js/script.js
+++ front/js/script.js
@@ -1,37 +1,41 @@
-var test = document.getElementById('productTest');
-let obj ;
-console.log(test);
-
-fetch('http://localhost:3000/api/products')
-  .then(response => response.json())
-  .then(data => {
-    // Traitement des données 
-    console.log('VAR DATA',data[0]);
-    data.forEach(item => {
-        test.innerHTML = `${item.name} ID ${item._id}`
-      }); 
-      for (const i in data) {
-           obj = data[i];
-
-          const {_id,name,price,imageUrl,colors} = obj;
-          console.log("ID:::",_id,'NAME :::',name,"PRIX :::",price);
-          
-          test.innerHTML = `ID /// ${_id} name /// ${name},PRIX:::${price},${imageUrl},${colors[0]}`
-          
-      
-      }
-      
-  })
-  .catch(error => {
-    // Gestion des erreurs 
-    console.log('erreur :',error);
-  });
-  console.log('SORTIE DE FETCH',obj);
+// All products
+async function getAllProducts(){
+    var response = await fetch('http://localhost:3000/api/products');
+    var AllProducts = await response.json();
+    return console.log(AllProducts);
+}
+getAllProducts(1)
 
 
 
 
 
+// Test Template home
+const sectionElement = document.createElement("section");
+const linkProductItems = document.createElement("a");
+const articleItem = document.createElement("article");
+const ImgItem = document.createElement("img");
+const TitleItem = document.createElement('h3');
+const ParagrapheItems = document.createElement('p');
 
+// Add Attribut for items
+linkProductItems.setAttribute('href',"https://test.com");
+sectionElement.setAttribute("id","items");
+sectionElement.setAttribute("class","items");
+ImgItem.setAttribute("src","./");
+ImgItem.setAttribute("alt","text");
+TitleItem.setAttribute('class',"productName")
+ParagrapheItems.setAttribute('class','productDescription')
 
+// Ajout des balises dans les balises nécessaire.
+sectionElement.appendChild(linkProductItems);
+linkProductItems.appendChild(articleItem);
+articleItem.appendChild(ImgItem);
+articleItem.appendChild(TitleItem);
+articleItem.appendChild(ParagrapheItems)
 
+//Add text 
+ParagrapheItems.innerText = 'Product';
+
+// Ajout de notre template
+document.body.appendChild(sectionElement);
