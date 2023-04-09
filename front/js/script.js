7b36d544878d5fe7453c2739ad4fa5c759622820
Axesan <82097764+Axesan@users.noreply.github.com>
2023-04-09 16:34:54 +0200

    REVIEW FUNCTIONS

--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,13 @@
-// All products
-export async function getAllProducts() {
-  var response = await fetch("http://localhost:3000/api/products");
-  var AllProducts = await response.json();
-  //AllProducts.forEach(data => { console.log('DATA::: ',data);});
-  return AllProducts;
-}
-// on export notre fonction pour la rendre accessible a tous nos autre fichiers
-export {getAllProducts};
+/**
+ * 
+ * @param {*} hrefLink Liens de l'image
+ * @param {*} nameProduct Nom du produits
+ * @param {*} altTxtImg Text Alternatif de l'image 
+ * @param {*} imgUrl Parametre Url 
+ * @param {*} descriptionProduct description du produits 
+ */
 
-async function templateData(data) {
-
-  const products = await data; // récupère la liste de produits
-
-  // boucle à travers chaque produit pour les traiter individuellement
-  products.forEach((product) => {
+function Template(hrefLink,nameProduct,altTxtImg,imgUrl,descriptionProduct) {
     // Template
     const elemSection = document.getElementById("items");
     const elemLinkProduct = document.createElement("a");
@@ -22,17 +16,16 @@ async function templateData(data) {
     const elemTitle = document.createElement("h3");
     const elemParagraph = document.createElement("p");
 
-    // Décomposition de variables
-    const { _id, name, price, altTxt, colors, imageUrl, description } = product;
-    // On traitre les données
-    elemLinkProduct.setAttribute("href", `product.html?id=${_id}`);
+    elemLinkProduct.setAttribute("href", hrefLink);
     elemLinkProduct.addEventListener("click", (event) => {
-      event.preventDefault(); // empêche le lien de charger une nouvelle page
-      window.location.href = elemLinkProduct.getAttribute("href"); // redirige vers la page que l'on a attribué comme attribut
-    });
-    elemImg.setAttribute("src", `${imageUrl}`);
-    elemTitle.innerText = name;
-    elemParagraph.innerText = description;
+        event.preventDefault(); // empêche le lien de charger une nouvelle page
+        window.location.href = elemLinkProduct.getAttribute("href"); // redirige vers la page que l'on a attribué comme attribut
+      });
+
+      elemImg.setAttribute("src", imgUrl);
+      elemImg.setAttribute("alt", altTxtImg);
+      elemTitle.innerText = nameProduct;
+      elemParagraph.innerText = descriptionProduct;
 
     // On ajoute une balise "a" a notre section
     elemSection.appendChild(elemLinkProduct);
@@ -42,9 +35,28 @@ async function templateData(data) {
     elemArticle.appendChild(elemImg);
     elemArticle.appendChild(elemTitle);
     elemArticle.appendChild(elemParagraph);
-  });
 }
-//Appel a la function templateData qui attend un arguments 'data'
 
-templateData(getAllProducts());
+/**
+* getAllProducts() -> Affiche nos elements a partir du template
+*/
+
+async function getAllProducts() {
+
+  var response = await fetch("http://localhost:3000/api/products");
+  var AllProducts = await response.json();
+  
+    AllProducts.forEach(data => {
+        // Décomposition de variable    
+        const { _id, name, altTxt, imageUrl, description } = data;
+        //Appel a la fonctions
+        Template(`product.html?id=${_id}`,name,altTxt,imageUrl,description)
+    });
+        
+}
+
+getAllProducts()
+
+
+
 
