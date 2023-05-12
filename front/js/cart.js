b43a4321e379fefcc09afbb7906a1fdd1dd6f4a8
Axesan <82097764+Axesan@users.noreply.github.com>
2023-05-12 14:31:16 +0200

    Review button order if none product in cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,32 +9,30 @@ async function getProducts(id) {
 
 async function getCart() {
   // Selections de notre boutons command
-  const buttonOrder = document.querySelector("#order")
-
+  const buttonOrder = document.querySelector("#order");
 
   // Vérifie si la variable "cart" contient des données
   if (cart && cart.length > 0) {
     // Parcours chaque élément du tableau "cart"
     for (var i = 0; i < cart.length; i++) {
-      TemplateCart(cart[i]);
+      var allDataCart = cart[i];
+      TemplateCart(allDataCart);
     }
   } else {
-   // On retire le boutons si le panier est vide .
-    buttonOrder.style.display = 'none';
+    // On retire le boutons si le panier est vide .
+    buttonOrder.style.display = "none";
   }
 }
 
-
-
-async function TemplateCart(dataCart){
+async function TemplateCart(dataCart) {
   // Decomposition de variable:
-  console.log("Data Cart",dataCart);
-  // Decomposition de variable de notre panier 
-  const {qty,color} = await dataCart
-  const {_id,name,price,imageUrl} = await getProducts(dataCart.id);
+  console.log("Data Cart", dataCart);
+  // Decomposition de variable de notre panier
+  const { qty, color } = await dataCart;
+  const { _id, name, price, imageUrl } = await getProducts(dataCart.id);
 
   let selectSection = document.querySelector("#cart__items");
-  let selectTotauPrice = document.querySelector('#totalPrice');
+  let selectTotauPrice = document.querySelector("#totalPrice");
   let elemArticle = document.createElement("article");
   let elemDivImg = document.createElement("div");
   let elemDivContent = document.createElement("div");
@@ -52,7 +50,6 @@ async function TemplateCart(dataCart){
   // SELECT ALL VALUE
   let deleteProduct = document.createElement("p");
   let nbArticle = document.querySelector("#totalQuantity");
- 
 
   // Ajout de balise article dans section
   selectSection.appendChild(elemArticle);
@@ -75,7 +72,7 @@ async function TemplateCart(dataCart){
   elemArticle.setAttribute("data-color", color);
 
   elemDivImg.setAttribute("class", "cart__item__img");
- 
+
   elemImg.setAttribute("alt", "{product-color}");
   elemDivContentDescription.setAttribute(
     "class",
@@ -97,7 +94,7 @@ async function TemplateCart(dataCart){
   deleteProduct.setAttribute("class", "deleteItem");
 
   // Ajout du textes
-  elemImg.setAttribute("src",imageUrl);
+  elemImg.setAttribute("src", imageUrl);
   inputQtyProduct.value = qty;
   titleProduct.innerText = name;
   priceProduct.innerText = price + "€";
@@ -106,42 +103,38 @@ async function TemplateCart(dataCart){
   qtyProduct.innerText = "Qté:";
   deleteProduct.innerText = "Supprimer";
 
- // Qty total + total price 
-  nbArticle.innerText = qtyTotalCart();  
-  selectTotauPrice.innerText = await totalPriceCart() ;
-
+  // Qty total + total price
+  nbArticle.innerText = qtyTotalCart();
+  selectTotauPrice.innerText = await totalPriceCart();
 
   //- DELETE ITEMS FROM CART
-  let deleteItemCart = document.querySelectorAll('.deleteItem');
- // BOUCLE SUR TOUS LES BOUTONS 
- // Parcourez tous les boutons "Supprimer" et ajoutez un gestionnaire d'événements click à chacun
- deleteItemCart.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    // Récupérez l'article parent du bouton "Supprimer" pour obtenir l'ID et la couleur du produit
-    const article = event.target.closest('.cart__item');
-    const productId = article.getAttribute('data-id');
-    const productColor = article.getAttribute('data-color');
+  let deleteItemCart = document.querySelectorAll(".deleteItem");
+  // Parcourez tous les boutons "Supprimer" et ajoutez un gestionnaire d'événements click à chacun
+  deleteItemCart.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      // Récupérez l'article parent du bouton "Supprimer" pour obtenir l'ID et la couleur du produit
+      const article = event.target.closest(".cart__item");
+      const productId = article.getAttribute("data-id");
+      const productColor = article.getAttribute("data-color");
 
-    // Supprimez le produit du panier en créant un nouveau panier sans cet article
-    let cart = JSON.parse(localStorage.getItem('cart'));
-    cart = cart.filter((product) => product.id !== productId || product.color !== productColor);
-    localStorage.setItem('cart', JSON.stringify(cart));
-
-    location.reload()
-    // Mettez à jour l'affichage du panier
-    //displayCart();
+      // Supprimez le produit du panier en créant un nouveau panier sans cet article
+      let cart = JSON.parse(localStorage.getItem("cart"));
+      cart = cart.filter(
+        (product) => product.id !== productId || product.color !== productColor
+      );
+      localStorage.setItem("cart", JSON.stringify(cart));
+      // Rafraichis la pages
+      location.reload();
+      
+    });
   });
-});
-
-
-
-  
+    
 }
 
 /** - GESTION DES ERREUR
-* @param {string} message
-* @param {string} selectBaliseMessage
-*/
+ * @param {string} message
+ * @param {string} selectBaliseMessage
+ */
 
 function displayErrorMessage(message, selectBaliseMessage) {
   // Affiche un message d'erreur pour l'input spécifié
@@ -149,7 +142,6 @@ function displayErrorMessage(message, selectBaliseMessage) {
   errorMsg.textContent = message;
   return false;
 }
-
 
 /**
  * VERIF FORMS
@@ -179,86 +171,58 @@ function VerifForms() {
 
   buttonForm.addEventListener("click", function () {
     if (!dataRegexp.firstname[0].test(selectNameInput.value)) {
-      displayErrorMessage(
-        
-        dataRegexp.firstname[1],
-        "#firstNameErrorMsg"
-      );
+      displayErrorMessage(dataRegexp.firstname[1], "#firstNameErrorMsg");
     }
 
     if (!dataRegexp.lastname[0].test(selectFirstNameInput.value)) {
-      displayErrorMessage(
-        
-        dataRegexp.lastname[1],
-        "#lastNameErrorMsg"
-      );
+      displayErrorMessage(dataRegexp.lastname[1], "#lastNameErrorMsg");
     }
     if (!dataRegexp.address[0].test(selectAddressInput.value)) {
-      displayErrorMessage(
-        
-        dataRegexp.address[1],
-        "#addressErrorMsg"
-      );
+      displayErrorMessage(dataRegexp.address[1], "#addressErrorMsg");
     }
     if (!dataRegexp.ville[0].test(selectCityInput.value)) {
-      displayErrorMessage(
-        
-        dataRegexp.ville[1],
-        "#cityErrorMsg"
-      );
+      displayErrorMessage(dataRegexp.ville[1], "#cityErrorMsg");
     }
     if (!dataRegexp.email[0].test(selectEmailInput.value)) {
-      displayErrorMessage(
-        
-        dataRegexp.email[1],
-        "#emailErrorMsg"
-      );
+      displayErrorMessage(dataRegexp.email[1], "#emailErrorMsg");
     }
   });
 }
 
+async function changeTotalPrice() {
 
-
-function changeTotalPrice() {
-  
-  // Récupérer la valeur de la clé "maCle"
-  var maValeur = localStorage.getItem("qty");
-  console.log(cart.length);
-
-// Afficher la valeur récupérée
-console.log("Function ///",maValeur);
 }
 
-
-function qtyTotalCart(){
-
+function qtyTotalCart() {
   // Initialiser une variable de total à 0
   let totalArticleQuantity = 0;
 
   // Parcourir la liste des produits et ajouter la quantité de chaque produit au total
-  cart.forEach(product => {
-    totalArticleQuantity += product.qty; 
+  cart.forEach((product) => {
+    totalArticleQuantity += product.qty;
   });
   return totalArticleQuantity;
 }
 /**
- * 
+ *
  * @returns totalPriceCart
  */
-async function totalPriceCart(){
-    // Initialiser une variable de total à 0 - TOTAL PRICES
-    let totalPriceCart = 0;
+async function totalPriceCart() {
+  // Initialiser une variable de total à 0 - TOTAL PRICES
+  let totalPriceCart = 0;
 
-   for (const product of cart) {
-     const { price } = await getProducts(product.id);
-     totalPriceCart += price * product.qty;
-     console.log('TOTAL PANIER: ' + totalPriceCart);
-   }
-   return totalPriceCart;
-  
+  for (const product of cart) {
+    const { price } = await getProducts(product.id);
+    totalPriceCart += price * product.qty;
+    console.log("TOTAL PANIER: " + totalPriceCart);
+  }
+  return totalPriceCart;
 }
 
-// Appel a nos fonctions 
+
+
+
+// Appel a nos fonctions
 getCart();
 //changeTotal()
 VerifForms();
